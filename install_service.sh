5c7eee7e85475b8f59f1ae3a040386588a84cf6a
Steve Lalonde <lalondesteve@gmail.com>
2020-06-27 22:09:17 +0100

    updated install script

--- install_service.sh
+++ install_service.sh
@@ -5,29 +5,27 @@ echo "Installing requirements"
 pip3 install -r $PWD/requirements.txt
 
 # set variables
-export THISUSER=$SUDO_USER THISFILE=$PWD/telnet_osc_forwarder.py
-HERE=$PWD/oscforwarder.service
+export THISUSER=$SUDO_USER THISFILE=$PWD/telnet_osc_forwarder.py HERE=$PWD/scripts/oscforwarder.service
 
 # create service file
 SERVVARS='$THISUSER:$THISFILE'
-envsubst "$SERVVARS" <service_template >oscforwarder.service
+envsubst "$SERVVARS" <$PWD/scripts/service_template >$HERE
 #
 # check if link exists
-THERE=/etc/systemd/system/oscforwarder.service
-if [ -f "$THERE" ]; then
+export THERE=/etc/systemd/system/oscforwarder.service
+if [ -L "$THERE" ]; then
 	echo "Removing exisiting link"
 	rm "$THERE"
 fi
 # #
 # create link
-echo "creating service symlink"
+echo "creating service symlink and making service persistent"
 ln -s "$HERE" "$THERE"
 # echo "$HERE" "$THERE"
 # initiate service
 systemctl daemon-reload
 service oscforwarder start
 # make service persistent
-echo "making service persistent"
 systemctl enable oscforwarder
-echo "installation complete"
+echo "installation script complete"
 # 
